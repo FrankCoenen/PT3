3f5a900bbc9adb3a350a0e1f9f52892c760dbec0
MysteryMerijn <mystery_merijn@live.nl>
2014-12-03 11:00:57 +0100

    V2 toegevoegd nieuwe interfacces voor lobby client

--- ProftaakPongV2/src/Server/components/ChatBox.java
+++ ProftaakPongV2/src/Server/components/ChatBox.java
@@ -4,13 +4,7 @@
  * and open the template in the editor.
  */
 
-package classes;
-
-import java.util.ArrayList;
-import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.ListView;
+package server.components;
 
 /**
  *
@@ -18,37 +12,5 @@ import javafx.scene.control.ListView;
  */
 public class ChatBox
 {
-    private ObservableList<ChatBericht> observableBerichten;
-    private List<ChatBericht> berichten;
-    private int maxberichten;
-    
-    public ChatBox()
-    {
-        maxberichten = 100;
-        berichten = new ArrayList<ChatBericht>();
-        observableBerichten = FXCollections.observableList(berichten);     
-    }
-    
-    public ObservableList<ChatBericht> getBerichten()
-    {
-        return (ObservableList<ChatBericht>) FXCollections.unmodifiableObservableList(observableBerichten);
-    }
-        
-    public void addBericht(ChatBericht c)
-    {
-        if(maxberichten == 0)
-        {
-            berichten.remove(10);
-        }
-        
-        observableBerichten.add(c);
-        System.out.println("Bericht geplaatst in lijst");
-        System.out.println("Bericht: " + c.getTekst());
-        maxberichten--;
-    }
-    
-    public int getListCount()
-    {
-        return berichten.size();
-    }
+   
 }
--- ProftaakPongV2/src/Server/components/Lobby.java
+++ ProftaakPongV2/src/Server/components/Lobby.java
@@ -4,89 +4,27 @@
  * and open the template in the editor.
  */
 
-package classes;
+package server.components;
 
-import database.DatabaseMediator;
+
 import java.util.ArrayList;
+import shared.interfaces.IClient;
+import shared.interfaces.IGame;
+import shared.interfaces.ILobbyLogin;
+import shared.interfaces.ILobbySignedIn;
+import shared.observer.RemotePublisher;
 
 /**
  *
  * @author Michael
  */
-public class Lobby 
+public class Lobby implements ILobbyLogin
 {
-    private ChatBox chatbox;
-    private ArrayList<Persoon> ingelogden;
-    private Persoon persooninapplicatie;
-    private DatabaseMediator database;
-    private ArrayList<GameLobby> gamelobbies;
-    private GameLobby joinedgamelobby;
-    
-    public Lobby(Persoon persooninapplicatie)
-    {
-        this.persooninapplicatie = persooninapplicatie;
-        gamelobbies = new ArrayList<>();
-        this.chatbox = new ChatBox();
+
+    @Override
+    public ILobbySignedIn login(IClient client, String naam, String wachtwoord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public Lobby()
-    {
-        
-    }
-    
-    public GameLobby getActiveGameLobby()
-    {
-        return this.joinedgamelobby;
-    }
-    
-    public DatabaseMediator getDatabase()
-    {
-        return this.database;
-    }
-    
-    public void setDatabase(DatabaseMediator database)
-    {
-        this.database = database;
-    }
-    
-    public void createGame()
-    {
-        GameLobby newgame = new GameLobby(persooninapplicatie.getGebruikersnaam() + " :Game", persooninapplicatie.omzettenSpeler().omzettenSpelEigenaar());
-        gamelobbies.add(newgame);
-        this.joinGame(newgame);
-    }
-    
-    public void startGame()
-    {
-        joinedgamelobby.startGame();
-    }
-    
-    public void joinGame(GameLobby gamelobby)
-    {
-        joinedgamelobby = gamelobby;
-    }
-    
-    public void spectateGame()
-    {
-        Toeschouwer spectater = persooninapplicatie.omzettenToeschouwer();
-    }
-    
-    public boolean inloggen()
-    {
-        return false;
-    }
-    
-    public void createChatBericht(String text, Persoon p)
-    {
-        ChatBericht bericht = new ChatBericht(text, p);
-        System.out.println("ChatBericht Object Aangemaakt met Text: " + bericht.getTekst());
-        this.chatbox.addBericht(bericht);
-        System.out.println("ChatBericht toegevoegd aan ChatBox Observable List");
-        System.out.println("List Count: " + this.chatbox.getListCount());
-    }
-    
-    public ChatBox getChatBox()
-    {
-        return this.chatbox;
-    }
-}
+
+   
+} 
